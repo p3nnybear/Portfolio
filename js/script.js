b5260b1086cec90012e79d444339c7890397e762
penny <p3nnydesign@gmail.com>
2019-08-19 23:22:38 -0700

    13

--- js/script.js
+++ js/script.js
@@ -52,3 +52,25 @@ var chart1 = new Chart(ctx, {
       }
     }
 });
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+      labels: ["Clothing", "Electronic product", "Books", "Cosmetic",  "Food", "Flight Supplies", "Home Supplies", "Photography stuffs", "household objects", "misc"],
+      datasets: [
+        {
+          label: "items bought online",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [40,32,20,19,15,1,1,1,1,1]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'What kind of items would you buy online?'
+      }
+    }
+});
